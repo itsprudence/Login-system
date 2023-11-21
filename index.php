7edf165b31d8fde2b1c79ce0e54722234c9dd836
Mercy-Nzambi <146635465+Mercy-Nzambi@users.noreply.github.com>
2023-11-22 00:04:44 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+include("connection.php");
+include("functions.php");
+
+?>
+
+
+
+<DOCTYPE html>
+    <html>
+        <head>
+            <title>My Website</title>
+        </head>
+        <body>
+
+        <a href="logout.php">Log Out</a>
+        <h1>This is the index page</h1>
+
+        <br>
+        Hello, username
+        </body>
+    </html>
